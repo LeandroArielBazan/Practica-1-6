c8fd0b26e599e68106094aa712f2d0f6f90ec041
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-03-31 19:22:46 -0300

    practica 1-6 terminada

--- algoritmo.js
+++ algoritmo.js
@@ -1,5 +1,20 @@
-let MellamoLeandro = prompt ("Cual es tu nombre?") 
+/* let MellamoLeandro = prompt ("Cual es tu nombre?") 
 
 console.log (MellamoLeandro);
 document.querySelector("body").innerHTML = "<h1>" + MellamoLeandro + "</h1>"
-document.write("<h2>" + MellamoLeandro + "</h2)");
+document.write("<h2>" + MellamoLeandro + "</h2)"); */
+
+
+let MellamoLeandro = prompt ("cual es tu nombre");
+let numeroEntero = prompt ("ingresar numero entero"); 
+let numeroDecimal = prompt ("ingresar numero decimal" );
+let booleano = prompt ("ingresar un dato booleano");
+let caracter = prompt ("ingresar un caracter");
+let palabra = prompt ("ingresar una palabra");
+
+document.write ("<h1> Entero:" + entero + "</h1>");
+document.write ("<h2> Numero decimal:" + numeroDecimal + "</h2>");
+document.write ("<h3> Booleano:" + booleano +"</h3>")  ;
+document.write ("<h4> caracter:" + caracter + "</h4>");
+document.write ("<h5> palabra:" + palabra + "</h5>");
+
