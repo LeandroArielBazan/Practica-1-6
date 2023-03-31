5a651ebe7bee6760528332586c9036a7b2218c54
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-03-31 13:14:26 -0300

    parte 1 de practica 1-6

---
+++ algoritmo.js
@@ -0,0 +1,5 @@
+let MellamoLeandro = prompt ("Cual es tu nombre?") 
+
+console.log (MellamoLeandro);
+document.querySelector("body").innerHTML = "<h1>" + MellamoLeandro + "</h1>"
+document.write("<h2>" + MellamoLeandro + "</h2)");
