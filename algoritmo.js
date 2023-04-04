ed6a115f44545ebb753388bd0657dc8dfe7d984b
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-04-04 17:59:09 -0300

    practica 1-6 corregida

--- algoritmo.js
+++ algoritmo.js
@@ -1,20 +1,12 @@
-/* let MellamoLeandro = prompt ("Cual es tu nombre?") 
+let  mellamoLeandro  =  prompt  ( "cual es tu nombre" ) ;
+let  numeroEntero  =  prompt  ( "ingresar numero entero" ) ; 
+let  numeroDecimal  =  prompt  ( "ingresar numero decimal"  ) ;
+let  booleano  =  prompt  ( "ingresar un dato booleano" ) ;
+let  caracter  =  prompt  ( "ingresar un caracter" ) ;
+let  palabra  =  prompt  ( "ingresar una palabra" ) ;
 
-console.log (MellamoLeandro);
-document.querySelector("body").innerHTML = "<h1>" + MellamoLeandro + "</h1>"
-document.write("<h2>" + MellamoLeandro + "</h2)"); */
-
-
-let MellamoLeandro = prompt ("cual es tu nombre");
-let numeroEntero = prompt ("ingresar numero entero"); 
-let numeroDecimal = prompt ("ingresar numero decimal" );
-let booleano = prompt ("ingresar un dato booleano");
-let caracter = prompt ("ingresar un caracter");
-let palabra = prompt ("ingresar una palabra");
-
-document.write ("<h1> Entero:" + entero + "</h1>");
-document.write ("<h2> Numero decimal:" + numeroDecimal + "</h2>");
-document.write ("<h3> Booleano:" + booleano +"</h3>")  ;
-document.write ("<h4> caracter:" + caracter + "</h4>");
-document.write ("<h5> palabra:" + palabra + "</h5>");
-
+documento _ escribir  ( "<h1> numeroEntero:"  +  numeroEntero  +  "</h1>" ) ;
+documento _ write  ( "<h2> Numero decimal:"  +  numeroDecimal  +  "</h2>" ) ;
+documento _ escribir  ( "<h3> Booleano:"  +  booleano  + "</h3>" )   ;
+documento _ escribir  ( "<h4> caracter:"  +  caracter  +  "</h4>" ) ;
+documento _ escribe  ( "<h5> palabra:"  +  palabra  +  "</h5>" ) ;
